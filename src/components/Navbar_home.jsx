75b035505ebc7ab5bec5364445b7db5975640df6
Jiancardeus <jia.rivera@duocuc.cl>
2025-10-10 23:39:58 -0300

    Login Fixeado
    Login listo, funciona con las credenciales de admin admin, listo para ser integrado con una bd

--- src/components/Navbar_home.jsx
+++ src/components/Navbar_home.jsx
@@ -9,6 +9,16 @@ const Navbar = () => {
         <li className="navbar-item">Mazo</li>
         <li className="navbar-item">Biblioteca</li>
       </ul>
+<<<<<<< Updated upstream
+=======
+
+      <div style={{ display: 'flex', alignItems:'center', color: '#ffffffff' }}>
+        <span style={{ color: '#fffffff',marginRight: '10px' }}>Hola. {currentUser}</span>
+        <button onClick={onLogout} style={{ padding: '5px 10px', cursor: 'pointer' }}>
+          Salir
+        </button>
+      </div>
+>>>>>>> Stashed changes
     </nav>
   );
 };
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,11 +3,19 @@ import Navbar from './Navbar_home';
 import background from '../background/home_background.png';
 const Home = () => {
   return (
+<<<<<<< Updated upstream
     
     <div>
        
       <Navbar />
        <img src={background} alt="Background" style={{ width: '100%', height: 'auto' }} />
+=======
+    <div className='home-container'>
+      <Navbar currentUser={currentUser} onLogout={onLogout}/>
+      <img src={background} alt="Background" style={{ width: '100%', height: 'auto' }} />
+    
+      <p></p>
+>>>>>>> Stashed changes
     </div>
   );
 };
