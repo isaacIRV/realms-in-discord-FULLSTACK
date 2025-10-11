ccac84bfdcd6ea8299eeb88ff6ae4efdf707b967
isaacIRV <i.rivas@duocuc.cl>
2025-10-10 23:26:18 -0300

    actualizacion de home

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './Navbar_home';
-
+import background from '../background/home_background.png';
 const Home = () => {
   return (
+    
     <div>
+       
       <Navbar />
-      <p></p>
+       <img src={background} alt="Background" style={{ width: '100%', height: 'auto' }} />
     </div>
   );
 };
